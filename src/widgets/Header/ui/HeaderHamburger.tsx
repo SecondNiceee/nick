ed37dab14df59e2a1782917ca3c8d77739b81c40
SecondNiceee <col1596321@gmail.com>
2025-01-27 07:34:18 +0300

    fix build errors

--- src/widgets/Header/ui/HeaderHamburger.tsx
+++ src/widgets/Header/ui/HeaderHamburger.tsx
@@ -13,7 +13,7 @@ const HeaderHamburger = () => {
             </div>
             <nav className={`rounded-[20px] md:static absolute md:w-[unset] w-[90%] 480:w-[80%] sm:w-[50%] bottom-[-20px] translate-y-[100%] md:translate-x-0 md:translate-y-0 translate-x-[-50%] transition-[left] duration-500  ${isOpen ? "left-[50%]" : "left-[-240px]"}`}>
                     <ul className='flex md:flex-row flex-col w-[100%]  gap-[36px] lg:gap-[47px] py-[22px] px-[30px] lg:px-[56px] bg-white justify-center items-center rounded-[20px]'>
-                        {links.map((link) => <li className='text-p cursor-pointer text-black font-normal'>{link.value}</li>)}
+                        {links.map((link, i) => <li key={i} className='text-p cursor-pointer text-black font-normal'>{link.value}</li>)}
                     </ul>
             </nav>
         </>
